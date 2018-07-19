15273689b2f43c3f2f0fc98a3255c37a0983356f
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2018-07-19 16:53:42 -0400

    Add form to create new album

--- album/views.py
+++ album/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.shortcuts import  reverse
 
 from album.models import Album
 
@@ -13,3 +15,34 @@ class AlbumOverviewView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class AlbumCreateView(LoginRequiredMixin, CreateView):
+    model = Album
+
+    fields = ['artist_name', 'album_name', 'released_date', 'genre', 'length']
+
+    template_name = 'album/create_album.html'
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+
+        album = Album()
+        if form.is_valid():
+            album.artist_name = form.cleaned_data['artist_name']
+            album.album_name = form.cleaned_data['album_name']
+            album.released_date = form.cleaned_data['released_date']
+            album.length = form.cleaned_data['length']
+            album.added_by = request.user
+            album.save()
+            for item in form.cleaned_data['genre']:
+                album.genre.add(item)
+
+            return HttpResponseRedirect(self.get_success_url())
+
+        else:
+            return HttpResponseRedirect(reverse('album:create'))
+
+    def get_success_url(self):
+        return reverse('album:overview')
