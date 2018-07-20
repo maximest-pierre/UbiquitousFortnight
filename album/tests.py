5d750cfed5febab36bfa787deaaf80c7d8455d07
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2018-07-20 10:28:58 -0400

    Add album form test

--- album/tests.py
+++ album/tests.py
@@ -53,7 +53,7 @@ class RatingTest(TestCase):
         self.assertNotEqual(self.rating.user.username, "")
 
 
-class AlbumCreateForm(TestCase):
+class AlbumCreateFormTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
@@ -79,17 +79,126 @@ class AlbumCreateForm(TestCase):
         self.assertEqual(result.status_code, 200)
 
     def test_form_save_data(self):
-        data = urlencode({
+        data = {
             "artist_name": "Test",
             "album_name": "Test",
             "released_date": "2017-06-15",
             "length": 12,
             "genre": "1",
-        })
+        }
 
         response = self.client.post(
-            reverse("album:create"), data, follow=True, content_type="application/x-www-form-urlencoded"
+            reverse("album:create"), data, follow=True,
         )
         album = Album.objects.get(album_name="Test")
 
         self.assertIsNotNone(album)
+
+
+class RatingDeleteFormTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.admin = User.objects.create_superuser(
+            username="admin@example.com",
+            email="admin@example.com",
+            password="test1234"
+        )
+        cls.rating = RatingFactory()
+
+    def setUp(self):
+        self.client.login(username=self.admin.username, password="test1234")
+
+    def tearDown(self):
+        self.client.logout()
+
+    def test_access_to_form(self):
+        result = self.client.get(
+            reverse("album:delete_rating", args=(self.rating.id,)),
+            follow=True
+        )
+        self.assertContains(result, 'Delete')
+
+    def test_delete_form(self):
+        result = self.client.post(
+            reverse("album:delete_rating", args=(self.rating.id,)),
+            follow=True
+        )
+        self.assertRedirects(result, reverse('album:overview'), status_code=302)
+
+
+class RatingUpdateFormTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.admin = User.objects.create_superuser(
+            username="admin@example.com",
+            email="admin@example.com",
+            password="test1234"
+        )
+        cls.rating = RatingFactory()
+
+    def setUp(self):
+        self.client.login(username=self.admin.username, password="test1234")
+
+    def tearDown(self):
+        self.client.logout()
+
+    def test_access_to_form(self):
+        result = self.client.get(
+            reverse("album:update_rating", args=(self.rating.id,)),
+            follow=True
+        )
+        self.assertContains(result, 'Edit')
+
+    def test_update_form(self):
+
+        data = {
+            "rating": 8
+        }
+        result = self.client.post(
+            reverse("album:update_rating", args=(self.rating.id,)),
+            data,
+            follow=True
+        )
+
+        self.assertRedirects(result, reverse('album:overview'), status_code=302)
+
+
+class RatingCreateFormTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        cls.admin = User.objects.create_superuser(
+            username="admin@example.com",
+            email="admin@example.com",
+            password="test1234"
+        )
+
+        cls.album = AlbumFactory()
+
+    def setUp(self):
+        self.client.login(username=self.admin.username, password="test1234")
+
+    def tearDown(self):
+        self.client.logout()
+
+    def test_access_to_form(self):
+        result = self.client.get(
+            reverse("album:new_rate", args=(self.album.id,)),
+            follow=True
+        )
+        self.assertContains(result, 'Add')
+
+    def test_create_form(self):
+        data = {
+            "rating": 8
+        }
+        result = self.client.post(
+            reverse("album:new_rate", args=(self.album.id,)),
+            data,
+            follow=True
+        )
+
+        self.assertRedirects(result, reverse('album:overview'), status_code=302)
