5e12968cacedc53a4265eccc6881c2cf497e1d9b
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2018-07-20 08:52:26 -0400

    Test Album New Form

--- album/tests.py
+++ album/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
-from album.models import Genre
+from album.models import Album, Genre, Rating
 from album.factories import AlbumFactory, GenreFactory, RatingFactory
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
+from urllib.parse import urlencode
+
+import datetime
+
 
 # Create your tests here.
 
@@ -45,3 +51,45 @@ class RatingTest(TestCase):
 
     def test_user(self):
         self.assertNotEqual(self.rating.user.username, "")
+
+
+class AlbumCreateForm(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.admin = User.objects.create_superuser(
+            username="admin@example.com",
+            email="admin@example.com",
+            password="test1234"
+        )
+        cls.genre = GenreFactory()
+
+    def setUp(self):
+        self.client.login(username=self.admin.username, password="test1234")
+
+    def tearDown(self):
+        self.client.logout()
+
+    def test_access_to_form(self):
+        result = self.client.get(
+            reverse("album:create"),
+            follow=True
+
+        )
+        self.assertEqual(result.status_code, 200)
+
+    def test_form_save_data(self):
+        data = urlencode({
+            "artist_name": "Test",
+            "album_name": "Test",
+            "released_date": "2017-06-15",
+            "length": 12,
+            "genre": "1",
+        })
+
+        response = self.client.post(
+            reverse("album:create"), data, follow=True, content_type="application/x-www-form-urlencoded"
+        )
+        album = Album.objects.get(album_name="Test")
+
+        self.assertIsNotNone(album)
